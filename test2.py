a2c3b220063ee677e85e3cba07297301606a91e8
bhruszka <bartoszhruszka@gmail.com>
2017-06-18 01:10:46 +0200

    123

--- test2.py
+++ test2.py
@@ -1,10 +1,10 @@
 import ImageProcessing as ip
 import glob, os
-os.chdir("root:\\testSet")
+os.chdir("root\\testSet")
 i = 1
 for file in glob.glob("*.jpg"):
     print(i)
-    ip.processImage(file, "root:\\testResults\\", True)
+    ip.processImage(file, "root\\testResults\\", True)
     i += 1
 
 
